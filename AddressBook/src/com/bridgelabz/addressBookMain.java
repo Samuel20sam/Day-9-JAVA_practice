1862ae791220e132a9378d904a82a871f028e74c
Samuel_Elijah <samuel20sam@gmail.com>
2022-03-30 20:44:24 +0530

    Adding: multiple address book ability for contacts

--- AddressBook/src/com/bridgelabz/addressBookMain.java
+++ AddressBook/src/com/bridgelabz/addressBookMain.java
@@ -6,35 +6,29 @@ public class addressBookMain
 {
     public static void main(String[] args) {
         System.out.println("Welcome to your personal directory");
-        addressBookMain main = new addressBookMain();
-        main.operation();
-    }
-
-    private void operation(){
         addressBook contact = new addressBook();
-        contact.setUpInfo();
+        contact.inputInfo();
         contact.displayListItems();
 
-        int i;
-        int j = 0;
-        for (i = 1; i > j; i++) {
-            System.out.println("""
+        System.out.println("""
                     Press 1 to continue adding contacts
                     Press 2 to edit a contact
                     Press 3 to delete a contact
+                    Press 4 to add a new address book
                     Press 0 to exit the program""");
-            Scanner in = new Scanner(System.in);
-            int operation = in.nextInt();
-            if (operation == 1) {
-                contact.setUpInfo();
-                contact.displayListItems();
-            } else if (operation == 2) {
-                contact.editContact();
-            } else if (operation == 0) {
-                j = i + 2;
-            } else if (operation==3){
-                contact.deleteContact();
-            }
+        Scanner in = new Scanner(System.in);
+        int operation = in.nextInt();
+        if (operation == 1) {
+            contact.inputInfo();
+            contact.displayListItems();
+        } else if (operation == 2) {
+            contact.editContact();
+        } else if (operation==3){
+            contact.deleteContact();
+        } else if (operation == 4) {
+            contact.extra();
+        } else if (operation == 0) {
+            System.exit(0);
         }
     }
 }
